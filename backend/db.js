c66917da9fbeb66f19c627746ada942a2cc1d9ec
maijacita <92377463+maijacita@users.noreply.github.com>
2022-03-22 16:49:34 +0200

    yeehaw
    juu on

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
-	password:'Teletappi',
+	password:'helmikuu21',
 	database:'kvgwolt'
 });
 connection.connect(function(error){
--- frontti/src/components/Etusivu.js
+++ frontti/src/components/Etusivu.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import Searchbar from './Searchbar'
+import {useState, useEffect} from 'react';
 
 export default function Etusivu() {
+
+
+const [ravintola, setravintola] = useState([])
+
+useEffect(async() => {
+  const result = await fetch('http://localhost:3001/ravintola').then((res)=>
+  res.json()
+  )
+  setravintola(result)
+  console.log(result)
+}, [])
+
   return (
     <div className = "etusivu">
            <Searchbar/>
             <div className='ravintolaContainer'>
-
+              {ravintola.map(({Nimi, Osoite, Aukiolo, Kuva, Tyyppi, Hintataso, Arviointi}) => (
                 <div className='Items'>
-                    <h1>Mäkkäri</h1>
-
-            </div>
+                  {Kuva}
+                  <p>{Nimi}</p>
+                  <p>{Osoite}</p>
+                  <p>{Aukiolo}</p>
+                  <p>{Tyyppi}</p>
+                  <p>{Hintataso}</p>
+                  <p>{Arviointi}</p>
+                </div>
+              ))}
+                
         </div>
         </div>
   )

c577b4b14af819682a99b8c31380350ba0553df8
hezixx <80600972+JunnoTo@users.noreply.github.com>
2022-03-23 17:13:06 +0200

    Ravintoloitsija näkymän router fixaus

--- frontti/src/index.js
+++ frontti/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Routes, Route,} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Ravintoloitsija from './components/Ravintoloitsija';
 import reportWebVitals from './reportWebVitals';
-/*
+
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
@@ -14,6 +15,6 @@ ReactDOM.render(
   </BrowserRouter>  ,
   document.getElementById('root')
 );
-*/
+
 reportWebVitals();
 

23b3c52fd0b6c10a5eccb311d253876deca72b83
maijacita <92377463+maijacita@users.noreply.github.com>
2022-03-24 16:50:08 +0200

    searchbaria
    näkymää vähä

--- frontti/src/components/Searchbar.js
+++ frontti/src/components/Searchbar.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
-export default function Searchbar() {
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
 
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+export default function Searchbar() {
 
   return (  
   <div className='name'>
     <h1>Kvg Rafla</h1>
   <div className="searchBarContainer">
-     <div className="searchBarItems">Hae ravintolaa <input type="text"/></div>
+     <div className="searchBarItems">Hae ravintolaa <input type="search" id= "search" onKeyUp="Haku()"/></div>
         <button className="itemButtons" onClick>Etsi</button>
+  <ul id="myUL">
+  <li><a href="#">McDonalds</a></li>
+  <li><a href="#">Hesburger</a></li>
+
+  <li><a href="#">Arbello</a></li>
+  <li><a href="#">Quattro</a></li>
+</ul>
     </div>
-    <ul id= "hakemisto"></ul>
     </div>
   );
 }

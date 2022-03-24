f6a3b1ea2535b1ba208b1bd1db5272416b011333
maijacita <92377463+maijacita@users.noreply.github.com>
2022-03-24 22:04:09 +0200

    hakutuloksia

--- frontti/src/components/Searchbar.js
+++ frontti/src/components/Searchbar.js
@@ -1,38 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
 
 export default function Searchbar() {
+
+const [haku, sethaku] = useState([])
+
+useEffect(async() => {
+  const result = await fetch('http://localhost:3001/ravintola').then((res)=>
+  res.json()
+  )
+  haku(result)
+  console.log(result)
+}, [])
+
+
 
   return (  
   <div className='name'>
     <h1>Kvg Rafla</h1>
   <div className="searchBarContainer">
-     <div className="searchBarItems">Hae ravintolaa <input type="search" id= "search" onKeyUp="Haku()"/></div>
+  {haku.map(({nimi}) => (
+                <div className="searchBarContainer">
+                  <p>{nimi}</p>
+                </div>
+              ))}
+     <div className="searchBarItems">Hae ravintolaa <input type="search" id= "search"/></div>
         <button className="itemButtons" onClick>Etsi</button>
-  <ul id="myUL">
-  <li><a href="#">McDonalds</a></li>
-  <li><a href="#">Hesburger</a></li>
-
-  <li><a href="#">Arbello</a></li>
-  <li><a href="#">Quattro</a></li>
-</ul>
+    
     </div>
     </div>
   );

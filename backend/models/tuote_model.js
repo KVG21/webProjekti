351d7d5c19dbc76a2692a231f41c29d6aabb5a8c
hezixx <80600972+JunnoTo@users.noreply.github.com>
2022-03-25 20:48:08 +0200

    kategoria lisätty tuote_modeliin

--- backend/models/tuote_model.js
+++ backend/models/tuote_model.js
@@ -8,16 +8,16 @@ const tuote = {
         return db.query('select * from tuote', callback);
     }, 
     add: function(tuote, callback) {
-        return db.query('insert into tuote (nimi, kuvaus, hinta, kuva, ravintolaID) values(?,?,?,?,?)',
-        [tuote.nimi, tuote.kuvaus, tuote.hinta, tuote.kuva, tuote.ravintolaID], callback);
+        return db.query('insert into tuote (kategoria, nimi, kuvaus, hinta, kuva, ravintolaID) values(?,?,?,?,?,?)',
+        [tuote.kategoria, tuote.nimi, tuote.kuvaus, tuote.hinta, tuote.kuva, tuote.ravintolaID], callback);
     },
     delete: function(id, callback){
         return db.query('delete from tuote where idTuote=?',[id], callback);
     },
     update: function(idTuote, tuote, callback){
         return db.query(
-            'update tuote set nimi=?, kuvaus=?, hinta=?, kuva=? where idTuote=?',
-            [tuote.nimi, tuote.kuvaus, tuote.hinta, tuote.kuva, idTuote], callback);
+            'update tuote set kategoria=? nimi=?, kuvaus=?, hinta=?, kuva=? where idTuote=?',
+            [tuote.kategoria, tuote.nimi, tuote.kuvaus, tuote.hinta, tuote.kuva, idTuote], callback);
     }
 };
 module.exports = tuote;

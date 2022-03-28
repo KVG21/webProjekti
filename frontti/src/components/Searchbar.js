56c63802cb2c64e9c18fbbc663ae0590c4e9063a
maijacita <92377463+maijacita@users.noreply.github.com>
2022-03-28 11:24:01 +0300

    hakupalkkia

--- frontti/src/components/Searchbar.js
+++ frontti/src/components/Searchbar.js
@@ -1,31 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom"
 
-
 export default function Searchbar() {
-
-const [haku, sethaku] = useState([])
-
-useEffect(async() => {
-  const result = await fetch('http://localhost:3001/ravintola').then((res)=>
-  res.json()
-  )
-  haku(result)
-  console.log(result)
-}, [])
-
-
 
   return (  
   <div className='name'>
     <h1>Kvg Rafla</h1>
   <div className="searchBarContainer">
-  {haku.map(({nimi}) => (
-                <div className="searchBarContainer">
-                  <p>{nimi}</p>
-                </div>
-              ))}
-     <div className="searchBarItems">Hae ravintolaa <input type="search" id= "search"/></div>
+     <div className="searchBarItems">Hae ravintolaa <input type="search" id= "searchbar"/></div>
         <button className="itemButtons" onClick>Etsi</button>
                 <nav>
                 <button className="naviNappi"><Link className="naviNimi" to="/Ravintoloitsija">Ravintoloitsija</Link></button>   
---
+++ frontti/src/components/hakupalkki.js
@@ -0,0 +1,38 @@
+const hakutulos = document.getElementById('hakutulos');
+const searchbar = document.getElementById('Searchbar');
+let tulos = [];
+
+searchbar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+  const filteredhakuehto = tulos.filter((hakuehto) => {
+    return (
+        hakuehto.nimi.toLowerCase().includes(searchString)
+    );
+});
+displayhakuehto(filteredhakuehto);
+});
+
+const loadhakuehto = async () => {
+  try {
+      const res = await fetch('http://localhost:3001/ravintola');
+      tulos = await res.json();
+      displayhakuehto(tulos);
+  } catch (err) {
+      console.error(err);
+  }
+};
+
+const displayhakuehto = (hakuehdot) => {
+  const htmlString = hakuehdot
+      .map((hakuehto) => {
+          return `
+          <li class="hakuehto">
+              <h2>${hakuehto.name}</h2>
+          </li>
+      `;
+      })
+      .join('');
+  hakutulos.innerHTML = htmlString;
+};
+
+loadhakuehto();

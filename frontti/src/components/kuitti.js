34a9d6db2e5b87c1128e0a721eba623ecd6ca1ec
maijacita <92377463+maijacita@users.noreply.github.com>
2022-04-01 15:18:00 +0300

    kuitin fetch ja map
    alustettu, muokkauksia voi tehdä

---
+++ frontti/src/components/kuitti.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './Tuotesivu.js'
+
+const Kuitti = () => {
+    const [kuitti, setkuitti] = useState[()]
+
+    useEffect(async() => {
+        const result = await fetch('http://localhost:3001/historia').then((res)=>
+        res.json())
+        setkuitti(result)
+        console.log(kuitti)
+    }, [])
+
+
+    return (
+        <div className='kuittiContainer'>
+            {kuitti.map(({tilaushistoria}) => (
+                <div className='tilaushistoria'></div>
+            ))}
+        </div>
+    )
+}

6852b2244414f37d4ff37d8c5811982d4c45c936
NikoHytinkoski <t0hyni00@students.oamk.fi>
2022-04-12 10:05:26 +0300

    Update App.js

--- frontti/src/App.js
+++ frontti/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Etusivu from './components/Etusivu'
 import Ravintoloitsija from './components/Ravintoloitsija';
 import Rekisteroityminen from './components/Rekisteroityminen'
+import KirjautuminenSivu from './components/KirjautuminenSivu';
 import Receipt from './components/Receipt'
 import Tuotesivu from './components/Tuotesivu';
 import {BrowserRouter, Routes, Route,} from 'react-router-dom';
@@ -12,8 +13,9 @@ function App() {
     <BrowserRouter>
         <div>   
         <Routes>
-            <Route path="/" element={ <Rekisteroityminen/> }/>
+            <Route path="/" element={ <KirjautuminenSivu/> }/>
             <Route path="/Etusivu" element={ <Etusivu/> }/>
+            <Route path="/Rekisteroityminen" element={ <Rekisteroityminen/> }/>
 
             <Route path="/Tuotesivu" element={ <Tuotesivu/> }>
                 <Route path=":id" element={ <Tuotesivu /> }/>

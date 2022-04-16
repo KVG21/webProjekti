d06ff85398549f95c783a9c8c88c88b9335c5b0c
hezixx <80600972+JunnoTo@users.noreply.github.com>
2022-04-16 18:21:40 +0300

    herokun kanssa kikkailua
    ei toimi.

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 const express = require('express')
 const app = express()
 const port = 3000
-var http = require('http')
 
 const asiakasrouter = require('./routes/asiakas');
 const ravintolarouter = require('./routes/ravintola');
@@ -16,10 +15,6 @@ const historiarouter = require('./routes/historia');
 const kirjautuminenrouter = require('./routes/kirjautuminen')
 
 
-http.createServer(function (req, res){
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger('dev'));
@@ -33,7 +28,6 @@ app.use('/historia',historiarouter);
 app.use('/kirjautuminen',kirjautuminenrouter);
 
 app.use(express.static('build'))
-}).listen(process.env.PORT)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

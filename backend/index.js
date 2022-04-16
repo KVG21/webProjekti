99b2cc8db9e6dd50d563e676c316571aa76eed71
hezixx <80600972+JunnoTo@users.noreply.github.com>
2022-04-16 17:32:29 +0300

    hello world path poistettu

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.use('/asiakas',asiakasrouter);
 app.use('/ravintola',ravintolarouter);
 app.use('/tuote',tuoterouter);
